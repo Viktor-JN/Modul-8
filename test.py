cf02c88c4037b1cef1741af9cb5c763e126630ec
Viktor-JN <viktor.johanssonnygren@elev.ntig.se>
2023-11-30 13:29:25 +0100

    fsaga

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
 import datetime
-
-x = int(input("""Skriv in ett år, månad och dag och se hur lång tid det är tills dess
+from datetime import date
+import msvcrt
+from os import system
+while True:
+    system('cls')
+    x = int(input("""Skriv in ett år, månad och dag och se hur lång tid det är tills dess
 Börja med år, sen månad och sist dag\n"""))
-y = int(input())
-z = int(input())
+    y = int(input())
+    z = int(input())
 
-compare_date = datetime.datetime(x, y, z)
+    compare_date = date(x, y, z)
+    while True:
+        print(compare_date)
+        print("Är det korrekt (J)a eller (N)ej")
+        answer = msvcrt.getwch()
+        if answer.upper() == "J":
+            break
+        elif answer.answer() == "N":
+            break
+        else:
+            continue
+    if answer.upper() == "J":
+        break
+    elif answer.upper() == "N":
+        continue
 
-print(compare_date.strftime("%Y"), compare_date.strftime("%m"), compare_date.strftime("%d"))
-
-today = datetime.datetime.now()
-
-year = today.strftime("%Y")
-month = today.strftime("%m")
-day = today.strftime("%d")
-
-compare_year = (int(year)-x)*365
-compare_month = (int(month)-y)*30
-compare_day = int(day)-z
-total=compare_day+compare_month+compare_year
-print(f"Det är {total} dagars skillnad")
+today = date.today()
+delta_date = compare_date-today
+if delta_date > 0:
+    print(f"Det är {delta_date} dagars skillnad från idag")
+else: 
+    print(f"")
